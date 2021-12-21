321b5a9936f8d8a8fb38b7ac85a968febe83ebbf
abdelouahabb <alabdelouahab@gmail.com>
2021-12-21 21:15:40 +0100

    some comments would be helpful

--- cloud-vision/main.py
+++ cloud-vision/main.py
@@ -1,9 +1,3 @@
-'''
-google-cloud-storage
-google-cloud-vision
-protobuf
-'''
-
 import proto
 from google.cloud import vision, storage
 
@@ -11,9 +5,17 @@ vision_client = vision.ImageAnnotatorClient()
 storage_client = storage.Client()
 
 def hello_gcs(event, context):
+  # get the name of the bucket (without gs://)
   bucket = storage_client.bucket(event['bucket'])
+  # name the file, each image will have its own result
   blob = bucket.blob(event['name']+'_result.json')
+  # let's build the full image uri
   image_uri = "gs://" + event['bucket'] + "/" + event['name']
+  # we want to execute the code only when the user uploads the image
+  # here we can also use content types, this is a generic trick
+  # dont rely on image extension, we need something advanced to check
+  # if the file is a real image (its header), 
+  # but for the simplicity of the example, let's make things easy
   #if event['contentType'] in ['image/apng', 'image/avif', 'image/gif', 'image/jpeg', 'image/png', 'image/svg+xml', 'image/webp']:
   if image_uri.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
     response = vision_client.annotate_image({
@@ -21,6 +23,8 @@ def hello_gcs(event, context):
       'features': [{'type_': vision_client.Feature.Type.FACE_DETECTION}]
       })
     print("______here we go______")
+    # the result is a special type, we need to convert it to something readable
     texts = proto.Message.to_json(response)
+    # reupload the response result to the same bucket
     blob.upload_from_string(texts)
     print("______finished_______")
